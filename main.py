cf0a7750988a91dc9f1c5644a8ecf852ab8cb17b
Taylor Burgess <tayloraburgess@gmail.com>
2016-10-24 15:58:58 -0400

    Modify index.html AJAX request  Please enter the commit message for your changes. Lines starting

--- main.py
+++ main.py
@@ -3,28 +3,24 @@ from flask import Flask, render_template, make_response, jsonify
 
 app = Flask(__name__)
 
-def unicode_tweets(tweets):
-	#re.search("regex", tweets)
-	return tweets
-
 def get_tweets():
-	for _ in range(10):
-		json_string = requests.get('http://totes-random.herokuapp.com/rand')
-		# If there's an error on the Haskell server--which happens randomly, but with
-		# decent regularity--it will return an error string in the response. 'widgets'
-		# is an arbitrary keyword that will occur in a valid response, but not the error.
-		if (json_string.text.find('widgets') != -1):
-			return json_string.text
-	return "{}"
+    for _ in range(10):
+        res = requests.get('http://totes-random.herokuapp.com/rand')
+        # If there's an error on the Haskell server--which happens randomly, but with
+        # decent regularity--it will return an error string in the response. So loop
+        # 10 times, but if a valid response isn't returned by then, give up
+        if (res.status_code == 200):
+            return res.text
+    return "{}"
 
 @app.route('/', methods=['GET'])
 def main():
-	return render_template('index.html')
+    return render_template('index.html')
 
 @app.route('/tweets', methods=['GET'])
 def tweets():
-	tweets = get_tweets()
-	return tweets
+    tweets = get_tweets()
+    return tweets
 
 if __name__ == "__main__":
     if 'PORT' in os.environ:

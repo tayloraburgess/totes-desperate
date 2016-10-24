00078afb943bdf05956be605a7cc34d2ed3e4e9d
Taylor Burgess <tayloraburgess@gmail.com>
2016-10-24 15:38:09 -0400

    Change styles slightly
    Also, configure frontend to work with remote Haskell server.

--- main.py
+++ main.py
@@ -4,11 +4,17 @@ import json
 from flask import Flask, render_template, make_response, jsonify
 
 app = Flask(__name__)
-app.config["DEBUG"]=True
+
+def unicode_tweets(tweets):
+	#re.search("regex", tweets)
+	return tweets
 
 def get_tweets():
 	for _ in range(10):
 		json_string = requests.get('http://totes-random.herokuapp.com/rand')
+		# If there's an error on the Haskell server--which happens randomly, but with
+		# decent regularity--it will return an error string in the response. 'widgets'
+		# is an arbitrary keyword that will occur in a valid response, but not the error.
 		if (json_string.text.find('widgets') != -1):
 			return json_string.text
 	return "{}"
@@ -20,6 +26,7 @@ def main():
 @app.route('/tweets', methods=['GET'])
 def tweets():
 	tweets = get_tweets()
+	return tweets
 
 if __name__ == "__main__":
 	app.run()
